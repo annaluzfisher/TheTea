c762426971512a7f1d136fcbc06634a0bcc9e918
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-08-27 20:06:05 -0700

    proxy added to package json

--- app/controllers/api/shares_controller.rb
+++ app/controllers/api/shares_controller.rb
@@ -1,16 +1,22 @@
 class Api::SharesController < ApplicationController
 
-    def show
+  def show
     @share = Share.find(params[:id])
     if @share
       render json: @share
     else
       @share = 'oh no it wasnt found shit'
     end
+ 
   end
 
   def create
-    @share = Share.new
+    @share = Share.new[share_params]
+    console.log('in the shares controller, share_params', share_params)
+  end
+
+  def share_params
+    params.require[:share].permit[:id,:content]
   end
 
 end
